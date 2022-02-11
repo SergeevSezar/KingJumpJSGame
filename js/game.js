4703af065278f44caf56c7a2eb3df0ac373531d6
sergeev.sergey <sergeev.sezar@yandex.ru>
2022-02-11 16:12:04 +0900

    first JS Game

---
+++ js/game.js
@@ -0,0 +1,18 @@
+import mainScene from "./scences/mainScene.js"
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    parent: 'game',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: true,
+        }
+    },
+    scene: [mainScene]
+}
+
+new Phaser.Game(config)
